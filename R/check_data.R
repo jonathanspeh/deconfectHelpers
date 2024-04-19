15173d48993d17c861bd951275834db42b18261d
jonathanspeh <jspeh@gmx.de>
2024-04-19 16:43:38 +0200

    Added and updated descriptions

--- R/check_data.R
+++ R/check_data.R
@@ -1,6 +1,14 @@
-# Find suspicious names -----
-# Checks for gene names that look like excel converted them into dates
-
+#' Find gene names that excel converte to dates
+#'
+#' @param dat A data frame containing gene names as a column.
+#' @param gene_names A string specifying the column to check.
+#' @param index  A bool indicateing wether to return the column index of suspicious genes. Default is TRUE
+#'
+#' @return A tibble containing the suspicious genes
+#' @export
+#'
+#' @examples
+#' 2 + 2
 find_suspicious_names <- function(dat, gene_names, index = TRUE){
   stopifnot("gene_names must be a valid column name" = gene_names %in% colnames(dat),
             "index must be logical" = is.logical(index))
