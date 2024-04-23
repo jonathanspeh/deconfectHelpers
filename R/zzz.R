ffb62ce8b2e962565ba5c66854c5ea61a41c11d3
jonathanspeh <jspeh@gmx.de>
2024-04-23 17:14:34 +0200

    added call to activate new condaenv

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,4 @@
 .onLoad <- function(libname, pkgname){
-  tryCatch(reticulate::use_condaenv("cellanneal-r"),
-           error = function(e){
              #packageStartupMessage("Creating cellanneal-r conda environment and installing dependencies")
              reticulate::conda_create("cellanneal-r", "python==3.8")
              reticulate::conda_install("cellanneal-r", c("numpy==1.24", "scipy==1.9",
@@ -16,11 +14,11 @@
              reticulate::py_install("cellanneal-master/", pip = TRUE)
              file.remove("cellanneal.zip")
              unlink("cellanneal-master", recursive = TRUE)
-           })
-  reticulate::source_python(
-    system.file("python/run_cellanneal.py", package = "deconfectHelpers"),
-    envir = deconv_env
-  )
+             reticulate::use_condaenv("cellanneal-r")
+            reticulate::source_python(
+              system.file("python/run_cellanneal.py",
+                          package = "deconfectHelpers"),
+              envir = deconv_env)
 }
 
 
