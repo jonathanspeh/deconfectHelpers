c2572457b66d7b68d06c78aa88c68bffc57b19a3
jonathanspeh <jspeh@gmx.de>
2024-04-29 15:25:26 +0200

    Added function to standardise celltypes

--- R/process_deconvolution.R
+++ R/process_deconvolution.R
@@ -22,3 +22,48 @@ process_cellanneal <- function(cell_mix){
 }
 
 
+
+
+#' Funktion to unify the names of immunecells
+#'
+#' @param cell_names A vector of immune cell names
+#'
+#' @return A vector of standardised cell names
+#' @export
+#'
+#' @examples
+#' fix_cell_names(c("bcells", "T cells CD4", "T reg"))
+#'
+fix_cell_names <- function(cell_names){
+  stopifnot("cell_names must be character vector" = is.character(cell_names))
+
+  B_cells <- c("b cell", "bcell", "B_cell")
+  T_cells_CD4 <- c("CD4", "cd4")
+  T_cells_CD8 <- c("CD8", "cd8", "Cytotoxic T","ctl")
+  T_regs <- c("T regulatory", "Treg", "regulatory t", "T reg", "T_reg")
+  Monocytes <- c("mono", "classical monocytes")
+  Neutrophils <- c("Neutro", "Neutrophiles", "Neutrophile granulocyte", "granulocyte neutro")
+  conventional_DCs <- c("conventional dendritic", "dendritic cells conv", "cDC",
+                        "conventional dc","conventional_dc", "myeloid dendritic")
+  NK_cells <- c("natural killer cells", "NK cell", "NK_cell")
+
+  # patterns <- list(B_cells = B_cells, T_cells_CD4 = T_cells_CD4,
+  #                  T_cells_CD8 = T_cells_CD8, T_regs = T_regs,
+  #                  Monocytes = Monocytes, Neutrophils = Neutrophils,
+  #                  conventional_DCs = conventional_DCs)
+
+  cell_names_actual <- dplyr::case_when(
+    grepl(paste(B_cells, collapse = "|"), cell_names, ignore.case = TRUE) ~ "B_cells",
+    grepl(paste(T_cells_CD4, collapse = "|"), cell_names, ignore.case = TRUE) ~ "T_cells_CD4",
+    grepl(paste(T_cells_CD8, collapse = "|"), cell_names, ignore.case = TRUE) ~ "T_cells_CD8",
+    grepl(paste(T_regs, collapse = "|"), cell_names, ignore.case = TRUE) ~ "T_regs",
+    grepl(paste(Monocytes, collapse = "|"), cell_names, ignore.case = TRUE) ~ "Monocytes",
+    grepl(paste(Neutrophils, collapse = "|"), cell_names, ignore.case = TRUE) ~ "Neutrophils",
+    grepl(paste(conventional_DCs, collapse = "|"), cell_names, ignore.case = TRUE) ~ "conventional_DCs",
+    grepl(paste(NK_cells, collapse = "|"), cell_names, ignore.case = TRUE) ~ "NK_cells",
+    .default =  paste("Other, original was: ", cell_names))
+  cell_names_actual
+  }
+
+
+
--- tests/testthat/test-process_deconvolution.R
+++ tests/testthat/test-process_deconvolution.R
@@ -4,3 +4,16 @@ test_that("process_cellanneal turns cellanneal output in long format", {
   expect_equal(process_cellanneal(test_data), expected)
   expect_error(process_cellanneal(mtcars), "provide valid cellanneal result")
   })
+
+
+test_that("can standardise celltypes", {
+          cells <- c("b cells", "bcell", "b_cell", "Naive B Cell",
+                     "cd4", "T cellcD4", "cd8", "cytotoxic T lymphocyte",
+                     "neutro", "mono", "cdc")
+
+          standardised <- c("B_cells","B_cells","B_cells", "B_cells",
+                            "T_cells_CD4", "T_cells_CD4", "T_cells_CD8", "T_cells_CD8",
+                            "Neutrophils", "Monocytes", "conventional_DCs")
+          expect_equal(fix_cell_names(cells), standardised) })
+
+
