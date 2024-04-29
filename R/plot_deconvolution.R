551b85490891ae524dc5ad718d8309467ea2fd95
jonathanspeh <jspeh@gmx.de>
2024-04-29 17:08:32 +0200

    Added metrics to celltype correlation plots

--- R/plot_deconvolution.R
+++ R/plot_deconvolution.R
@@ -47,6 +47,8 @@ plot_deconvolution_violin <- function(deconvolution, actual, remove_missing_cell
 #' @param data a data frame containing estimated and actual fractions
 #' @param cell_type_filter a vector of celltypes to plot
 #' @param add_x_y_line Boolean to specify wether to plot red x == y line
+#' @param add_metrics boolean if RMSE and correlation should be added to plots
+#' @param append_title Character to add to ggtitle
 #'
 #' @return a plot of the selected celltype
 #' @importFrom ggplot2 .data
@@ -56,7 +58,10 @@ plot_deconvolution_violin <- function(deconvolution, actual, remove_missing_cell
 #' plot_corr_celltype(data, cell_type_filter)
 #' }
 
-plot_corr_celltype <- function(data, cell_type_filter, add_x_y_line = add_x_y_line){
+plot_corr_celltype <- function(data, cell_type_filter,
+                               add_x_y_line = add_x_y_line,
+                               add_metrics = TRUE,
+                               append_title = NULL){
 
   subs <- dplyr::filter(data, .data$cell_type == cell_type_filter)
 
@@ -68,16 +73,26 @@ plot_corr_celltype <- function(data, cell_type_filter, add_x_y_line = add_x_y_li
     ggplot2::ggplot(ggplot2::aes(x = .data$proportion_actual, y = .data$proportion_estimate)) +
     ggplot2::geom_point() +
     ggplot2::geom_smooth(method = "lm") +
-    ggplot2::ggtitle(cell_type_filter) +
+    ggplot2::ggtitle(paste(cell_type_filter, append_title)) +
     ggplot2::coord_cartesian(xlim = lim, ylim = lim)
 
   if(add_x_y_line){
-    p +
+    p <- p +
       ggplot2::geom_abline(slope = 1,
                   colour = "red")
-  } else {
-      p
-    }
+  }
+
+  if(add_metrics){
+      rmse <- sqrt(mean((subs$proportion_actual - subs$proportion_estimate)^2))
+      cors <- stats::cor.test(subs$proportion_actual, subs$proportion_estimate)
+      p <- p +
+        ggplot2::annotate("text", label = paste0("RMSE = ", round(rmse, 4), "\nR = ", round(cors$estimate, 2), "\nP =  ", round(cors$p.value, 4)),
+                          x = min(subs$proportion_estimate),
+                          y = max(subs$proportion_actual),
+                          hjust = 0,
+                          vjust = 0.7)
+  }
+
  }
 
 
@@ -88,6 +103,8 @@ plot_corr_celltype <- function(data, cell_type_filter, add_x_y_line = add_x_y_li
 #' @param actual actual fractions in long format
 #' @param add_x_y_line Boolean to specify wether to plot red x == y line
 #' @param remove_missing_celltypes A boolean weather to remove celltypes that are only in one of the datasets
+#' @param add_metrics boolean if RMSE and correlation should be added to plots
+#' @param append_title Character to add to ggtitle
 #'
 #' @return A plot for each cell_type in the datasets
 #' @export
@@ -99,7 +116,9 @@ plot_corr_celltype <- function(data, cell_type_filter, add_x_y_line = add_x_y_li
 
 plot_deconvolution_celltype_corrs <- function(deconvolution, actual,
                                               add_x_y_line = TRUE,
-                                              remove_missing_celltypes = TRUE){
+                                              remove_missing_celltypes = TRUE,
+                                              add_metrics = TRUE,
+                                              append_title = NULL){
 
   if(remove_missing_celltypes){
     deconv_only <- setdiff(deconvolution$cell_type, actual$cell_type)
@@ -119,7 +138,8 @@ plot_deconvolution_celltype_corrs <- function(deconvolution, actual,
                                  suffix = c("_estimate", "_actual"))
 
   cell_types <- unlist(unique(estimate_v_actual["cell_type"]))
-  lapply(cell_types, plot_corr_celltype, data = estimate_v_actual, add_x_y_line = add_x_y_line)
+  lapply(cell_types, plot_corr_celltype, data = estimate_v_actual, add_x_y_line = add_x_y_line,
+         add_metrics = add_metrics, append_title = append_title)
 
 }
 
