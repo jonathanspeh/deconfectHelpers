db75d873ec61e577856c9a5a967ab3cec268139e
jonathanspeh <jspeh@gmx.de>
2024-08-14 17:57:45 +0200

    Cleaned up old files

--- tests/testthat/test-python_calls.R
+++
@@ -1,9 +0,0 @@
-# test_that("run cellanneal works", {
-#   expected <- readRDS("testdata/cellanneal_result.Rdata")
-#   set.seed(42)
-#   actual <- run_cellanneal_r("testdata/signature.csv",
-#                              "testdata/mixture.csv")
-#   expect_equal(dim(actual), dim(expected))
-#   expect_equal(colnames(actual), colnames(expected))
-#   expect_equal(actual$sample, expected$sample)
-# })
